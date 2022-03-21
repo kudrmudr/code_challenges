159b28cb045f65da35b8c781c998253829762763
Constantin Plotkin <kudrikkk@yandex.ru>
2022-03-21 18:47:46 +0100

    go

---
+++ GoLang/order_by_weight.go
@@ -0,0 +1,40 @@
+// The solution setup is where you can provide the candidate with the basic framework for their solution.
+package challenge
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func calcWeight(numStr string) int {
+	weight := 0
+	for _, digitStr := range numStr {
+		digit, _ := strconv.Atoi(string(digitStr))
+		weight = weight + digit
+	}
+
+	return weight
+}
+
+func compareStrings(strI string, strJ string) bool {
+	wI := calcWeight(strI)
+	wJ := calcWeight(strJ)
+
+	if wI == wJ {
+		return strI < strJ
+	}
+
+	return wI < wJ
+}
+
+func OrderByWeight(strn string) string {
+
+	numbers := strings.Split(strn, " ")
+
+	sort.SliceStable(numbers, func(i, j int) bool {
+		return compareStrings(numbers[i], numbers[j])
+	})
+
+	return strings.Join(numbers, " ")
+}
