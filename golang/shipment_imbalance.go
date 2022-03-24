8d7a94a4bb0177fd84707e172ca028c532f1e390
Constantin Plotkin <kudrikkk@yandex.ru>
2022-03-24 21:59:46 +0100

    dsds

---
+++ golang/shipment_imbalance.go
@@ -0,0 +1,42 @@
+package main
+
+//O(N^2) solution
+func imbalance(weights []int) int {
+	var res int
+	l := len(weights)
+
+	for i, vi := range weights {
+
+		min := vi
+		max := 0
+		for j := i; j < l; j++ {
+			vj := weights[j]
+
+			if vj > max {
+				max = vj
+			}
+
+			if vj < min {
+				min = vj
+			}
+
+			res = res + max - min
+
+		}
+	}
+
+	return res
+}
+
+func main() {
+
+	println("res:", imbalance([]int{1, 2, 3}))
+
+	println("___")
+
+	println("res:", imbalance([]int{3, 2, 3}))
+
+	println("___")
+
+	println("res:", imbalance([]int{1, 8, 2, 1}))
+}
